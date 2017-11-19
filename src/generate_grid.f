415396b07510cb88b23022a0cd0f343640fb1d5c
Michiel Eggermont <michiel.eggermont@gmail.com>
2017-11-19 14:57:19 +0100

    Add generate_grid_new subroutine

--- src/generate_grid.f
+++ src/generate_grid.f
@@ -6,13 +6,12 @@ C ---------------------------------------------------------------------------
       INCLUDE 'knd_params.inc'
       INCLUDE 'net_params.inc'
       INCLUDE 'net_funcs.inc'
-      INCLUDE 'grd_params.inc'
       INCLUDE 'usr_params.inc'
 C
       INTEGER(KIND=IK) NNW               ! nr. of networks (IN)
       INTEGER(KIND=IK) NW_IPAR (N_IP, *) ! integer network params (IN)
       REAL   (KIND=RK) NW_RPAR (N_RP, *) ! real network params (IN)
-      REAL   (KIND=RK) CRD (4, *)        ! grid point coordinates (OUT)
+      REAL   (KIND=RK) CRD (2, 2, *)     ! grid point coordinates (OUT)
 C
       INTEGER(KIND=IK) INW, IGP, NGP, GRT
       REAL   (KIND=RK) GRID_PAR (N_GRP)
@@ -22,36 +21,49 @@ C     Set the grid point pointer to the first grid point
       IGP = 1
 C     Loop over all networks
       DO INW = 1, NNW
+C        Get the number of grid points and the grid type
          NGP = GET_NGP (NW_IPAR (1, INW))
          GRT = GET_GRT (NW_IPAR (1, INW))
          WRITE (USR_O, 101) INW, NGP
-C        Get the number of grid points and the grid type
 C        Get the grid parameters
          CALL GET_GRP (NW_RPAR (1, INW), GRID_PAR)
 C        Generate the grid depending on the value of GRT
-         IF (GRT .EQ. GRD_LINE) THEN
-            CALL LINE_GRID
-     &           (NGP, GRID_PAR, CRD (1, IGP))
-         ELSE IF (GRT .EQ. GRD_STRETCHED) THEN
-            CALL STRETCHED_GRID
-     &           (NGP, GRID_PAR, CRD (1, IGP))
-         ELSE IF (GRT .EQ. GRD_CIRCLESECT) THEN
-            CALL CIRCLESECT_GRID
-     &           (NGP, GRID_PAR, CRD (1, IGP))
-         ELSE IF (GRT .EQ. GRD_CUBIC) THEN
-            CALL CUBIC_GRID
-     &           (NGP, GRID_PAR, CRD (1, IGP))
-         ELSE
-            CALL ERROR ('GENERATE_GRID', 'Grid type not implemented')
-         END IF
+         CALL GENERATE_GRID_NW (NGP, GRT, GRID_PAR, CRD (1, 1, IGP))
 C        increment the grid pointer
          IGP = IGP + NGP
       END DO
       WRITE (USR_O, *) '========== GRID IS GENERATED ======'
 C
       RETURN
-  101 FORMAT (1X,'NETWORK',I3,', NGP =',I4)
+  101 FORMAT (1X, 'NETWORK', I3, ', NGP =', I4)
       END
+
+      SUBROUTINE GENERATE_GRID_NW (NGP, GRT, GRID_PAR, CRD)
+C ---------------------------------------------------------------------------
+C     Generates the initial grid for a network
+C ---------------------------------------------------------------------------
+      IMPLICIT NONE
+      INCLUDE 'knd_params.inc'
+      INCLUDE 'grd_params.inc'
+      INCLUDE 'net_params.inc'
+      INTEGER(KIND=IK) NGP
+C
+      INTEGER(KIND=IK) GRT
+      REAL   (KIND=RK) GRID_PAR (N_GRP)
+      REAL   (KIND=RK) CRD (2, 2, NGP)     ! grid point coordinates (OUT)
+C
+      IF (GRT .EQ. GRD_LINE) THEN
+        CALL LINE_GRID (NGP, GRID_PAR, CRD)
+      ELSE IF (GRT .EQ. GRD_STRETCHED) THEN
+        CALL STRETCHED_GRID (NGP, GRID_PAR, CRD)
+      ELSE IF (GRT .EQ. GRD_CIRCLESECT) THEN
+        CALL CIRCLESECT_GRID (NGP, GRID_PAR, CRD)
+      ELSE IF (GRT .EQ. GRD_CUBIC) THEN
+        CALL CUBIC_GRID (NGP, GRID_PAR, CRD)
+      ELSE
+        CALL ERROR ('GENERATE_GRID', 'Grid type not implemented')
+      END IF
+      END SUBROUTINE
 
       SUBROUTINE LINE_GRID (N, PAR, X)
 C ---------------------------------------------------------------------------
