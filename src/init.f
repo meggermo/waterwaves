56364b7d9b32d5b9a1c1cf6d506a162a0d2e6e78
Michiel Eggermont <michiel.eggermont@gmail.com>
2017-11-19 19:38:56 +0100

    Move check subroutines into init

--- src/init.f
+++ src/init.f
@@ -332,3 +332,176 @@ C
 C
       RETURN
       END
+
+      SUBROUTINE CHECK_INTERFACES
+     &           (NSD, NNW_SD, NW_IPAR)
+C ---------------------------------------------------------------------------
+C
+C ---------------------------------------------------------------------------
+      IMPLICIT NONE
+      INCLUDE 'knd_params.inc'
+      INCLUDE 'net_params.inc'
+      INCLUDE 'net_funcs.inc'
+C
+      INTEGER(KIND=IK) NSD
+      INTEGER(KIND=IK) NNW_SD (*)
+      INTEGER(KIND=IK) NW_IPAR (N_IP, *)
+C
+      INTEGER(KIND=IK) ISD, INW, NNW, NTF
+C
+      NNW = 0
+      DO ISD = 1, NSD
+         NNW = NNW + NNW_SD (ISD)
+      END DO
+      NTF = 0
+      DO INW = 1, NNW
+         IF (GET_ITF (NW_IPAR (1, INW)) .NE. 0) THEN
+            NTF = NTF + 1
+         END IF
+      END DO
+      IF (MOD (NTF, 2) .NE. 0) THEN
+         CALL ERROR ('CHECK_INTERFACES', 'Number of ITF''s is odd')
+      END IF
+      CALL CHECK_INTERF (NNW, NTF, NW_IPAR)
+      RETURN
+      END
+      SUBROUTINE CHECK_INTERF
+     &           (NNW, NTF, NW_IPAR)
+C ---------------------------------------------------------------------------
+C
+C ---------------------------------------------------------------------------
+      IMPLICIT NONE
+      INCLUDE 'knd_params.inc'
+      INCLUDE 'usr_params.inc'
+      INCLUDE 'net_params.inc'
+      INCLUDE 'net_funcs.inc'
+C
+      INTEGER(KIND=IK) NNW
+      INTEGER(KIND=IK) NTF
+      INTEGER(KIND=IK) NW_IPAR (N_IP, *)
+C
+      INTEGER(KIND=IK) INW, ITF, JTF, N_ITFS, TMP
+      INTEGER(KIND=IK) ITF_ID (NTF)
+      INTEGER(KIND=IK) INW_ID (NTF)
+C
+      N_ITFS = 0
+      DO INW = 1, NNW
+         ITF = GET_ITF (NW_IPAR (1, INW))
+         IF (ITF .NE. 0) THEN
+            N_ITFS = N_ITFS + 1
+            ITF_ID (N_ITFS) = ITF
+            INW_ID (N_ITFS) = INW
+         END IF
+      END DO
+      DO ITF = 1, N_ITFS
+         DO JTF = ITF + 1,  N_ITFS
+            IF (ITF_ID (JTF) .LT. ITF_ID (ITF)) THEN
+               TMP = ITF_ID (JTF)
+               ITF_ID (JTF) = ITF_ID (ITF)
+               ITF_ID (ITF) = TMP
+               TMP = INW_ID (JTF)
+               INW_ID (JTF) = INW_ID (ITF)
+               INW_ID (ITF) = TMP
+            END IF
+         END DO
+      END DO
+      DO ITF = 1, N_ITFS, 2
+C        IF (ITF_ID (ITF) .NE. ITF_ID (ITF + 1)) THEN
+            WRITE (USR_O, 101) ITF_ID (ITF),     INW_ID (ITF)
+            WRITE (USR_O, 101) ITF_ID (ITF + 1), INW_ID (ITF + 1)
+C           CALL ERROR ('CHECK_INTERF', 'WRONG INTERFACE ID''s')
+C        END IF
+      END DO
+C
+      RETURN
+  101 FORMAT (1X,'AT INTERFACE',I2,' OF NETWORK',I4)
+      END
+
+      SUBROUTINE CHECK_NETW_PAR
+     &           (ISD, NNW, NW_IPAR, NW_RPAR)
+C ---------------------------------------------------------------------------
+C
+C ---------------------------------------------------------------------------
+      IMPLICIT NONE
+      INCLUDE 'knd_params.inc'
+      INCLUDE 'net_params.inc'
+C
+      INTEGER(KIND=IK) ISD
+      INTEGER(KIND=IK) NNW
+      INTEGER(KIND=IK) NW_IPAR (N_IP, *)
+      REAL   (KIND=RK) NW_RPAR (N_RP, *)
+C
+      INTEGER(KIND=IK) INW, IED
+C
+      DO INW = 1, NNW
+         DO IED = 1, 2
+            CALL CHECK_CONNECTIONS
+     &           (ISD, INW, IED, NNW, NW_IPAR, NW_RPAR)
+         END DO
+      END DO
+C
+      RETURN
+      END
+      SUBROUTINE CHECK_CONNECTIONS
+     &           (ISD, INW, IED, NNW, NW_IPAR, NW_RPAR)
+C ---------------------------------------------------------------------------
+C
+C ---------------------------------------------------------------------------
+      IMPLICIT NONE
+      INCLUDE 'knd_params.inc'
+      INCLUDE 'usr_params.inc'
+      INCLUDE 'net_params.inc'
+      INCLUDE 'net_funcs.inc'
+C
+      INTEGER(KIND=IK) ISD
+      INTEGER(KIND=IK) INW
+      INTEGER(KIND=IK) IED
+      INTEGER(KIND=IK) NNW
+      INTEGER(KIND=IK) NW_IPAR (N_IP, *)
+      REAL   (KIND=RK) NW_RPAR (N_RP, *)
+C
+      INTEGER(KIND=IK) ANW, AED, BNW, BED
+      REAL   (KIND=RK) PAR_I (N_GRP)
+      REAL   (KIND=RK) PAR_A (N_GRP)
+C
+      ANW = GET_ANW (IED, NW_IPAR (1, INW))
+      IF (ANW .GT. NNW) THEN
+         WRITE (USR_O, 101) ISD
+         WRITE (USR_O, 111) IED, INW, ANW
+         CALL ERROR ('CHECK_NETW_IPAR', 'Network ID out of range')
+      END IF
+      AED = GET_AED (IED, NW_IPAR (1, INW))
+      BNW = GET_ANW (AED, NW_IPAR (1, ANW))
+      BED = GET_AED (AED, NW_IPAR (1, ANW))
+      IF (INW .NE. BNW) THEN
+         WRITE (USR_O, 101) ISD
+         WRITE (USR_O, 121) IED, INW, ANW
+         WRITE (USR_O, 121) AED, ANW, BNW
+         CALL ERROR ('CHECK_NETW_IPAR', 'Network ID mismatch')
+      END IF
+      IF (IED .NE. BED) THEN
+         WRITE (USR_O, 101) ISD
+         WRITE (USR_O, 121) IED, INW, ANW
+         WRITE (USR_O, 121) AED, ANW, BNW
+         CALL ERROR ('CHECK_NETW_IPAR', 'Edge ID mismatch')
+      END IF
+      CALL GET_GRP (NW_RPAR (1, INW), PAR_I)
+      CALL GET_GRP (NW_RPAR (1, ANW), PAR_A)
+      IF (NNW .NE. 1) THEN
+      IF (PAR_I (1 + 2 * (IED - 1)) .NE. PAR_A (1 + 2 * (AED -1)) .OR.
+     &    PAR_I (2 + 2 * (IED - 1)) .NE. PAR_A (2 + 2 * (AED -1))) THEN
+         WRITE (USR_O, 131) IED, INW, PAR_I (1 + 2 * (IED -1)),
+     &                                PAR_I (2 + 2 * (IED -1))
+         WRITE (USR_O, *) 'BUT'
+         WRITE (USR_O, 131) AED, ANW, PAR_A (1 + 2 * (AED -1)),
+     &                                PAR_A (2 + 2 * (AED -1))
+         CALL ERROR ('CHECK_NETW_IPAR', 'Coordinate mismatch')
+      END IF
+      END IF
+C
+      RETURN
+  101 FORMAT ('IN SUBDOMAIN',I2)
+  111 FORMAT ('EDGE',I2,' OF NETW',I4,' CANNOT CONNECT TO NETW',I4)
+  121 FORMAT ('EDGE',I2,' OF NETW',I4,' CONNECTS TO NETW',I4)
+  131 FORMAT ('EDGE',I2,' OF NETW',I4,' HAS COORDS',10E14.6)
+      END
