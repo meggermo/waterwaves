b4320a9545a0e85306abeb7e621b27feadb11b7f
Michiel Eggermont <michiel.eggermont@gmail.com>
2017-12-28 14:26:15 +0100

    Plot velocity vectors

--- resources/octave/plotout.m
+++ resources/octave/plotout.m
@@ -1,12 +1,19 @@
-function waveplot
+function plotout
 
 load dim.out;
-load crd.out; x = crd(:,1); z = crd(:,2);
-N=sum(dim);
-I=1:N;
-T=floor(size(crd,1)/N);
-for i=T-10:T-2,J=I+N*(i-1);
-  plot(x(J),z(J),'o-');
-  axis([0 40 -4 1]);
-  pause(1);
+load crd.out; 
+x = crd(:,1); z = crd(:,2); 
+u = crd(:,5); v = crd(:,6);
+N = sum(dim);
+I = 1:N;
+T = floor(size(crd,1)/N);
+cla;
+hold off;
+for i=1:T,J=I+N*(i-1);
+  cla;
+  hold on;
+  quiver(x(J), z(J), u(J), v(J));
+  plot(x(J), z(J), 'ro-');  
+  axis([0 45 -4 2]);
+  pause(0.02);
 end
