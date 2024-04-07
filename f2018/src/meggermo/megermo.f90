9f3a6d65926ca9426fa4d1ba538ec8ddb720533d
Michiel Eggermont <michiel.eggermont@gmail.com>
2024-04-07 10:43:35 +0200

    Add params

--- f2018/src/meggermo/megermo.f90
+++ f2018/src/meggermo/megermo.f90
@@ -1,2 +1,12 @@
 module meggermo
+
+   use, intrinsic :: iso_fortran_env, only: &
+      rk => real64, &
+      ik => int64
+
+   implicit none
+   private
+
+   public :: rk, ik
+
 end module
---
+++ f2018/src/meggermo/params/meggermo_params.f90
@@ -0,0 +1,19 @@
+module meggermo_params
+
+   use meggermo, only: rk
+
+   implicit none
+   public
+
+   real(kind=rk), parameter :: zero = 0.0
+   real(kind=rk), parameter :: one = 1.0
+   real(kind=rk), parameter :: two = 2.0
+   real(kind=rk), parameter :: three = 3.0
+   real(kind=rk), parameter :: four = 4.0
+   real(kind=rk), parameter :: five = 5.0
+   real(kind=rk), parameter :: six = 6.0
+   real(kind=rk), parameter :: eight = 8.0
+   real(kind=rk), parameter :: nine = 9.0
+   real(kind=rk), parameter :: sixteen = 16.0
+
+end module
